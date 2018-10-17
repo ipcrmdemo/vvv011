cf3484e3a04d018c134b0f26bf280f813a83f449
Matt Cadorette <mcadorette@gmail.com>
2018-10-17 13:10:35 -0400

    Initial commit from Atomist

---
+++ src/main/java/vvv011/Vvv011Application.java
@@ -0,0 +1,12 @@
+package vvv011;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vvv011Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vvv011Application.class, args);
+	}
+}
